1adc145971a069bc1595e0781ff4849e661d2b14
Stefan <steveg94@web.de>
2019-06-12 10:11:55 +0200

    API linear single leg running

--- Code/E_API_lin_single_leg.py
+++ Code/E_API_lin_single_leg.py
@@ -63,10 +63,11 @@ for row in settings:
 # variations (capacity and no purchase preference)
 if use_variations:
     var_capacities, var_no_purchase_preferences = get_variations(example)
+    preferences_no_purchase = var_no_purchase_preferences[0]
 else:
-    capacities, no_purchase_preferences = get_capacities_and_preferences_no_purchase(example)
+    capacities, preferences_no_purchase = get_capacities_and_preferences_no_purchase(example)
     var_capacities = np.array([capacities])
-    var_no_purchase_preferences = np.array([no_purchase_preferences])
+    var_no_purchase_preferences = np.array([preferences_no_purchase])
 
 # no varying capacity here
 capacities = var_capacities[0]
@@ -86,16 +87,15 @@ print("\nEverything set up.")
 K = 3  # 60
 I = 10  # 800
 
-# all parameters in one row of a dataframe
 # K+1 policy iterations (starting with 0)
 # T time steps
-# theta and bid price for each resource
-param = np.array([[np.zeros(1+len(resources))]*T]*(K+1))
-# pi for each time step
-pi_act = np.zeros(len(capacities))
+theta_all = np.array([[np.zeros(1)]*T]*(K+1))
+pi_all = np.array([[np.zeros(len(resources))]*T]*(K+1))
 
+# theta and pi for each time step
 # line 1
-param[0] = 0
+thetas = 0
+pis = np.zeros(len(resources))
 
 for k in np.arange(K)+1:
     np.random.seed(k)
@@ -115,12 +115,12 @@ for k in np.arange(K)+1:
             # line 7  (starting capacity at time t)
             c_sample[t] = c
 
-            # line 9  (adjust bid price)
-            pi_act[c_sample[t] == 0] = np.inf
-            pi_act[c_sample[t] > 0] = param[k-1][t][np.append(False, c_sample[t] > 0)]
+            # line 8-11  (adjust bid price)
+            pis[c_sample[t] == 0] = np.inf
+            pis[c_sample[t] > 0] = pi_all[k - 1][t][c_sample[t] > 0]
 
             # line 12
-            offer_set = determine_offer_tuple(pi_act)
+            offer_set = determine_offer_tuple(pis)
 
             # line 13
             customer = int(np.random.choice(np.arange(len(arrival_probabilities)),
@@ -130,7 +130,7 @@ for k in np.arange(K)+1:
                                         size=1,
                                         p=customer_choice_individual(offer_set,
                                                                      preference_weights[customer],
-                                                                     preference_no_purchase[customer])))
+                                                                     preferences_no_purchase[customer])))
             # line 14
             try:
                 r_sample[t] = revenues[sold]
@@ -144,7 +144,7 @@ for k in np.arange(K)+1:
         c_samples[i] = c_sample
 
     # line 20
-    param[k] = update_parameters(v_samples, c_samples, param[k-1])
+    theta_all[k], pi_all[k] = update_parameters(v_samples, c_samples, theta_all[k-1], pi_all[k-1])
 
 
 #%%
@@ -163,39 +163,39 @@ def determine_offer_tuple(pi):
     offer_tuple = np.zeros_like(revenues)
 
     # line 1
-    S_prime = revenues - pi > 0  # vector - scalar = vector - np.ones_like(vector)*scalar
-    if all(np.invert(S_prime)):
+    s_prime = revenues - pi > 0  # vector - scalar = vector - np.ones_like(vector)*scalar
+    if all(np.invert(s_prime)):
         return offer_tuple
 
     # line 2-3
     # offer_sets_to_test has in each row an offer set, we want to test
-    offer_sets_to_test = np.zeros((sum(S_prime), len(revenues)))
-    offer_sets_to_test[np.arange(sum(S_prime)), np.where(S_prime)] = 1
+    offer_sets_to_test = np.zeros((sum(s_prime), len(revenues)))
+    offer_sets_to_test[np.arange(sum(s_prime)), np.where(s_prime)] = 1
     offer_sets_to_test += offer_tuple
     offer_sets_to_test = (offer_sets_to_test > 0)
 
     value_marginal = np.apply_along_axis(calc_value_marginal, axis=1, arr=offer_sets_to_test)
 
     offer_tuple[np.argmax(value_marginal)] = 1
-    S_prime = S_prime & offer_tuple == 0
-    v_S = np.amax(value_marginal)
+    s_prime = s_prime & offer_tuple == 0
+    v_s = np.amax(value_marginal)
 
     # line 4
     while True:
         # 4a
         # offer_sets_to_test has in each row an offer set, we want to test
-        offer_sets_to_test = np.zeros((sum(S_prime), len(revenues)))
-        offer_sets_to_test[np.arange(sum(S_prime)), np.where(S_prime)] = 1
+        offer_sets_to_test = np.zeros((sum(s_prime), len(revenues)))
+        offer_sets_to_test[np.arange(sum(s_prime)), np.where(s_prime)] = 1
         offer_sets_to_test += offer_tuple
         offer_sets_to_test = (offer_sets_to_test > 0)
 
         # 4b
         value_marginal = np.apply_along_axis(calc_value_marginal, axis=1, arr=offer_sets_to_test)
 
-        if np.amax(value_marginal) > v_S:
-            v_S = np.amax(value_marginal)
+        if np.amax(value_marginal) > v_s:
+            v_s = np.amax(value_marginal)
             offer_tuple[np.argmax(value_marginal)] = 1
-            S_prime = S_prime & offer_tuple == 0
+            s_prime = s_prime & offer_tuple == 0
             if all(offer_tuple == 1):
                 break
         else:
@@ -207,65 +207,65 @@ def calc_value_marginal(indices_inner_sum):
     v_temp = 0
     for l in np.arange(len(preference_weights)):
         v_temp += arrival_probabilities[l] * \
-                  sum(indices_inner_sum * (revenues - np.apply_along_axis(sum, 1, A.T * pi)) * preference_weights[l, :]) / \
+                  sum(indices_inner_sum * (revenues - np.apply_along_axis(sum, 1, A.T * pis)) *
+                      preference_weights[l, :]) / \
                   (sum(indices_inner_sum * preference_weights[l, :]) + var_no_purchase_preferences[l])
     return v_temp
 
 
-def update_parameters(v_samples, c_samples, params):
+def update_parameters(v_samples, c_samples, thetas, pis):
     """
     Updates the parameter theta, pi given the sample path using least squares linear regression with constraints.
     Using gurobipy
 
     :param v_samples:
     :param c_samples:
-    :param params:
     :return:
     """
     set_i = np.arange(len(v_samples))
-    set_t = np.arange(len(times))
-    set_c = np.arange(len(pi[0]))
+    set_t = np.arange(len(thetas))
+    set_c = np.arange(len(pis[0]))
 
     theta_multidict = {}
     for t in set_t:
-        theta_multidict[t] = theta[t]
+        theta_multidict[t] = thetas[t]
     theta_indices, theta_tuples = multidict(theta_multidict)
 
     pi_multidict = {}
     for t in set_t:
         for c in set_c:
-            pi_multidict[t, c] = pi[t, c]
+            pi_multidict[t, c] = pis[t, c]
     pi_indices, pi_tuples = multidict(pi_multidict)
 
     try:
         m = Model()
 
         # Variables
-        mTheta = m.addVars(theta_indices, name="theta", lb=0.0)  # Constraint 10
-        mPi = m.addVars(pi_indices, name="pi", ub=max(revenues))  # Constraint 11
+        m_theta = m.addVars(theta_indices, name="theta", lb=0.0)  # Constraint 10
+        m_pi = m.addVars(pi_indices, name="pi", ub=max(revenues))  # Constraint 11
 
         for t in set_t:
-            mTheta[t].start = theta_tuples[t]
+            m_theta[t].start = theta_tuples[t]
             for c in set_c:
-                mPi[t, c].start = pi_tuples[t, c]
+                m_pi[t, c].start = pi_tuples[t, c]
 
-        # Goal Function
-        lse = quicksum((v_samples[i][t] - mTheta[t] - quicksum(mPi[t, c] * c_samples[t][c] for c in set_c)) *
-                       (v_samples[i][t] - mTheta[t] - quicksum(mPi[t, c] * c_samples[t][c] for c in set_c))
-                       for i in set_i for t in set_t)
+        # Goal Function (14)
+        lse = quicksum((v_samples[i][t] - m_theta[t] - quicksum(m_pi[t, c] * c_samples[t][c] for c in set_c)) *
+                       (v_samples[i][t] - m_theta[t] - quicksum(m_pi[t, c] * c_samples[t][c] for c in set_c))
+                       for t in set_t for i in set_i)
         m.setObjective(lse, GRB.MINIMIZE)
 
         # Constraints
         # C12 (not needed yet)
         for t in set_t[:-1]:
-            m.addConstr(mTheta[t], GRB.GREATER_EQUAL, mTheta[t+1], name="C15")  # Constraint 15
+            m.addConstr(m_theta[t], GRB.GREATER_EQUAL, m_theta[t+1], name="C15")  # Constraint 15
             for c in set_c:
-                m.addConstr(mPi[t, c], GRB.GREATER_EQUAL, mPi[t+1, c], name="C16")  # Constraint 16
+                m.addConstr(m_pi[t, c], GRB.GREATER_EQUAL, m_pi[t+1, c], name="C16")  # Constraint 16
 
         m.optimize()
 
-        theta_new = deepcopy(theta)
-        pi_new = deepcopy(pi)
+        theta_new = copy.deepcopy(thetas)
+        pi_new = copy.deepcopy(pis)
 
         for t in set_t:
             theta_new[t] = m.getVarByName("theta[" + str(t) + "]").X
