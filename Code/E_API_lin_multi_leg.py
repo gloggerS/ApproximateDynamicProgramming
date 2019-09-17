036d9dc4379f114c33a8e97845f2d0931518b0a3
Stefan <steveg94@web.de>
2019-09-17 10:24:56 +0200

    adp with plots

--- Code/E_API_lin_multi_leg.py
+++ Code/E_API_lin_multi_leg.py
@@ -17,6 +17,9 @@ logfile, newpath, var_capacities, var_no_purchase_preferences, resources, produc
 capacities = var_capacities[0]
 preferences_no_purchase = var_no_purchase_preferences[0]
 
+# %% to store workspace
+import shelve
+filename = newpath+"\\shelve.out"
 
 #%%
 def update_parameters(v_samples, c_samples, thetas, pis, k, exponential_smoothing):
@@ -97,12 +100,17 @@ theta_result = {}
 value_result = {}
 capacities_result = {}
 
-# generate the random sample paths
+# generate the random sample paths (T+1 => have real life indexing starting at t=1)
 np.random.seed(12)
 seed(12)
 customer_stream = np.random.random((I, T+1))
 eps_random = np.random.random((I, T+1))
 sales_stream = np.random.random((I, T+1))
+
+# summary statistics
+offer_sets_all = {str(tuple(obj)):count for count, obj in enumerate(get_offer_sets_all(products))}
+offersets_offered = np.zeros((K+1, len(offer_sets_all)))
+products_sold = np.zeros((K+1, len(products)+1))
 
 for capacities in var_capacities:
     value_result[str(capacities)] = {}
@@ -114,8 +122,8 @@ for capacities in var_capacities:
         print(capacities, "of", str(var_capacities.tolist()), " - and - ",
               preferences_no_purchase, "of", str(var_no_purchase_preferences.tolist()), "starting.")
 
-        value_result[str(capacities)][str(preferences_no_purchase)] = {}
-        capacities_result[str(capacities)][str(preferences_no_purchase)] = {}
+        value_result[str(capacities)][str(preferences_no_purchase)] = np.zeros((K+1, I, T+1))
+        capacities_result[str(capacities)][str(preferences_no_purchase)] = np.zeros((K+1, I, T+1, len(resources)))
 
         # store parameters over all policy iterations
         # K+1 policy iterations (starting with 0)
@@ -132,13 +140,13 @@ for capacities in var_capacities:
             print(k, "of", K, "starting.")
             seed(13)  # to have the epsilon's (exploration vs exploitation) also the same for each policy iteration k
 
-            v_samples = np.zeros((I, len(times)+1))
-            c_samples = np.zeros((I, len(times)+1, len(capacities)))
+            v_samples = np.zeros((I, T+1))
+            c_samples = np.zeros((I, T+1, len(capacities)))
 
             for i in np.arange(I):
                 # line 3
-                r_sample_i = np.zeros(len(times)+1)  # will become v_sample
-                c_sample_i = np.zeros(shape=(len(times)+1, len(capacities)), dtype=int)
+                r_sample_i = np.zeros(T+1)  # will become v_sample
+                c_sample_i = np.zeros(shape=(T+1, len(capacities)), dtype=int)
 
                 # line 5
                 c = deepcopy(capacities)  # (starting capacity at time 0)
@@ -154,10 +162,12 @@ for capacities in var_capacities:
                     # line 12  (epsilon greedy strategy)
                     offer_set = determine_offer_tuple(pis, epsilon[k], eps_random[i, t], revenues, A,
                                                       arrival_probabilities, preference_weights, preferences_no_purchase)
+                    offersets_offered[k, offer_sets_all[str(offer_set)]] += 1
 
                     # line 13  (simulate sales)
                     sold = simulate_sales(offer_set, customer_stream[i, t], sales_stream[i, t],
                                           arrival_probabilities, preference_weights, preferences_no_purchase)
+                    products_sold[k, sold] += 1
 
                     # line 14
                     try:
@@ -174,8 +184,8 @@ for capacities in var_capacities:
             # line 20
             theta_all[k], pi_all[k] = update_parameters(v_samples, c_samples, theta_all[k-1], pi_all[k-1], k, exponential_smoothing)
             
-            value_result[str(capacities)][str(preferences_no_purchase)][k] = v_samples
-            capacities_result[str(capacities)][str(preferences_no_purchase)][k] = c_samples
+            value_result[str(capacities)][str(preferences_no_purchase)][k, :, :] = v_samples
+            capacities_result[str(capacities)][str(preferences_no_purchase)][k, :, :] = c_samples
 
         theta_result[str(capacities)][str(preferences_no_purchase)] = theta_all
         pi_result[str(capacities)][str(preferences_no_purchase)] = pi_all
@@ -214,3 +224,48 @@ with open(newpath+"\\piToUse.data", "wb") as filehandle:
 
 # %%
 wrapup(logfile, time_start, newpath)
+
+# %%
+my_shelf = shelve.open(filename, "n")  # "n" for new
+for key in dir():
+    try:
+        my_shelf[key] = globals()[key]
+    except TypeError:
+        #
+        # __builtins__, my_shelf, and imported modules can not be shelved.
+        #
+        print('ERROR shelving: {0}'.format(key))
+    except Exception as e:
+        print("Error {0}".format(e))
+my_shelf.close()
+
+# %% restore
+my_shelf = shelve.open(filename)
+for key in my_shelf:
+    globals()[key] = my_shelf[key]
+my_shelf.close()
+
+
+# %% analysis
+# products sold
+p = pd.DataFrame(products_sold, columns=range(len(products)+1))
+p = p.iloc[1:, :]
+
+fig, ax = plt.subplots()
+for i in products:
+    ax.plot(np.arange(K)+1, p[i], label="Product "+str(i+1))
+ax.plot(np.arange(K)+1, p[i+1], label="No Purchase")
+ax.legend()
+plt.xticks(np.arange(K)+1)
+plt.show()
+
+#%%
+v = pd.DataFrame(value_result[str(capacities)][str(preferences_no_purchase)][:, :, 0])
+v = v.iloc[1:, :]
+v.apply(sum, axis=1)
+
+np.mean(v, axis=1)
+
+plt.plot(np.arange(K)+1, np.mean(v, axis=1), label="Average value at start")
+plt.xticks(np.arange(K)+1)
+plt.show()
